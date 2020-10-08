4e76504d96a760b90c9533cb982ffbc805791517
Yash <yash.gupta5961@gmail.com>
2020-10-08 15:49:26 +0530

    Fixed config.txt to make learning easier. Add save winner feature to training file and improved fitness function

--- learn.py
+++ learn.py
@@ -4,6 +4,8 @@ import os
 import random
 import math
 import neat
+import visualize
+import pickle
 
 WIN_WIDTH = 450
 WIN_HEIGHT = 800
@@ -193,6 +195,7 @@ def evaluate(genomes, config):
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    score -= 200
                     run = False
                     pygame.quit()
                     quit()
@@ -203,6 +206,7 @@ def evaluate(genomes, config):
             if control:
                 output = net.activate((hero.x, stick.length, baselist[1].x, baselist[1].x + baselist[1].width))
                 if output[0] > 0.5 and output[1] > 0.5:
+                    score -= 1000
                     run = False
                     break
                 if output[0] > 0.5 and growing:
@@ -237,7 +241,7 @@ def evaluate(genomes, config):
             if not walk and not rotating and not growing and not pushback and stick.length != 0:
                 if stick.xEnd < baselist[1].x or stick.xEnd > baselist[1].x + baselist[1].width: 
                   # print("You Died! Score:", score)
-                    score = -1000
+                    score = -100
                     run = False
                 if stick.xEnd > baselist[1].redX and stick.xEnd < baselist[1].redX + 4: 
                     score += 200
@@ -290,10 +294,20 @@ def run(configF):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     
-    #Fix vvvv 
+
     winner = p.run(evaluate, 50) 
 
     print('\nBest genome:\n{!s}'.format(winner))
+
+
+    with open("winner.pkl", "wb") as f:
+        pickle.dump(winner, f)
+        f.close()
+
+    # visualize.draw_net(config, winner, True)
+    # visualize.plot_stats(stats, ylog=False, view=True)
+    # visualize.plot_species(stats, view=True)
+
 
                         
             
@@ -302,3 +316,65 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
     run(config_path)
+
+
+
+
+# Best genome:
+# Key: 1312
+# Fitness: 331111.7600001454
+# Nodes:
+#         0 DefaultNodeGene(key=0, bias=-0.720352058251976, response=1.0, activation=tanh, aggregation=sum)
+#         1 DefaultNodeGene(key=1, bias=1.2687336469143369, response=1.0, activation=tanh, aggregation=sum)
+# Connections:
+#         DefaultConnectionGene(key=(-3, 0), weight=-1.4427143753428666, enabled=True)
+#         DefaultConnectionGene(key=(-2, 0), weight=1.569481408191497, enabled=True)
+#         DefaultConnectionGene(key=(-2, 1), weight=-2.1376340942343783, enabled=True)
+
+
+# Best genome:
+# Key: 668
+# Fitness: 184402.02000002644
+# Nodes:
+#         0 DefaultNodeGene(key=0, bias=-1.6858809903771177, response=1.0, activation=tanh, aggregation=sum)
+#         1 DefaultNodeGene(key=1, bias=3.0569849838153136, response=1.0, activation=tanh, aggregation=sum)
+# Connections:
+#         DefaultConnectionGene(key=(-4, 0), weight=-1.070286974635163, enabled=True)
+#         DefaultConnectionGene(key=(-4, 1), weight=1.3013792382379978, enabled=False)
+#         DefaultConnectionGene(key=(-3, 0), weight=-1.7531069638580985, enabled=True)
+#         DefaultConnectionGene(key=(-3, 1), weight=1.2013431325014778, enabled=True)
+#         DefaultConnectionGene(key=(-2, 0), weight=3.1296213168454274, enabled=True)
+#         DefaultConnectionGene(key=(-2, 1), weight=-2.9150201981524093, enabled=True)
+#         DefaultConnectionGene(key=(-1, 0), weight=1.300531069772046, enabled=True)
+
+# Best genome:
+# Key: 239
+# Fitness: 156351.48999999507
+# Nodes:
+#         0 DefaultNodeGene(key=0, bias=2.1138385389659033, response=1.0, activation=tanh, aggregation=sum)
+#         1 DefaultNodeGene(key=1, bias=1.4959330298776585, response=1.0, activation=tanh, aggregation=sum)
+# Connections:
+#         DefaultConnectionGene(key=(-4, 0), weight=-0.2666502862921748, enabled=True)
+#         DefaultConnectionGene(key=(-4, 1), weight=-0.10640464531546145, enabled=True)
+#         DefaultConnectionGene(key=(-3, 0), weight=-1.876862918599099, enabled=True)
+#         DefaultConnectionGene(key=(-2, 0), weight=2.431347916319419, enabled=True)
+#         DefaultConnectionGene(key=(-2, 1), weight=-2.784877750959131, enabled=True)
+#         DefaultConnectionGene(key=(-1, 1), weight=2.2762330742299195, enabled=True)
+
+# Best genome:
+# Key: 507
+# Fitness: 109295.31999998237
+# Nodes:
+#         0 DefaultNodeGene(key=0, bias=-0.42993651008735817, response=1.0, activation=tanh, aggregation=sum)
+#         1 DefaultNodeGene(key=1, bias=-1.937339845734826, response=1.0, activation=tanh, aggregation=sum)
+#         79 DefaultNodeGene(key=79, bias=-0.4971173457604932, response=1.0, activation=tanh, aggregation=sum)
+# Connections:
+#         DefaultConnectionGene(key=(-4, 0), weight=-0.573689671630509, enabled=False)
+#         DefaultConnectionGene(key=(-4, 1), weight=-0.9013653309807369, enabled=False)
+#         DefaultConnectionGene(key=(-4, 79), weight=-0.14321700492240885, enabled=True)
+#         DefaultConnectionGene(key=(-3, 0), weight=-3.3654559949989222, enabled=True)
+#         DefaultConnectionGene(key=(-3, 1), weight=0.23021451925727204, enabled=True)
+#         DefaultConnectionGene(key=(-2, 0), weight=4.052481358065705, enabled=True)
+#         DefaultConnectionGene(key=(-2, 1), weight=-1.8383539253072518, enabled=True)
+#         DefaultConnectionGene(key=(-1, 0), weight=0.20659698615214542, enabled=True)
+# Format: "svg" not recognized. Use one of:
